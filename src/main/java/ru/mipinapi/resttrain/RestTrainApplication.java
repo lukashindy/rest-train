3358b4c0186b14b56d7b3d44f77c94ecf7ef7355
rudoy-spec <mipinapi@gmail.com>
2020-08-29 23:56:58 +0300

    Initial commit

---
+++ src/main/java/ru/mipinapi/resttrain/RestTrainApplication.java
@@ -0,0 +1,13 @@
+package ru.mipinapi.resttrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mipinapi/resttrain/RestTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mipinapi.resttrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
